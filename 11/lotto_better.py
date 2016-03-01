ce00b478fda8a496007db406da1dc1342305e639
Laura Bolčina <laurabolcina@Lauras-MacBook-Pro.local>
2016-03-01 23:35:43 +0100

    Added docstrings

--- 11/lotto_better.py
+++ 11/lotto_better.py
@@ -4,6 +4,7 @@ __author__ = "Laura Bolčina"
 import random
 
 def lotto(n):
+    """ Generates a list of n numbers, no duplicates """
     a = random.sample(range(1, 40), n)
     return a
 
--- 11/lotto_better_test.py
+++ 11/lotto_better_test.py
@@ -1,8 +1,9 @@
 from lotto_better import lotto
 
 def testing_lotto():
+    """ Checks if generated list and generated list with no duplicates are the same length. If they are, the function passes successfully. """
     generated_list = lotto(7)
-    assert len(generated_list) == len(set(generated_list))
+    assert len(generated_list) == len(set(generated_list))   # set() creates an unordered list with no duplicate elements
     return "testing_lotto() passed successfully"
 
 print testing_lotto()
